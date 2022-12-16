a6150b6df70a9b3c16cc94963bbdd1c5ca06f66c
iakshay28 <120718682+iakshay28@users.noreply.github.com>
2022-12-16 16:29:51 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program that accepts a word from the user and reverse it.
+
+# In[2]:
+
+
+name = input("enter a name ")
+revstr=""
+for i in name:
+    revstr=i+revstr
+print(revstr)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ ASSIGNMENT _1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to count the number of even and odd numbers from a series of numbers.
+
+# In[3]:
+
+
+numbers=eval(input("enter numbers"))
+even_count, odd_count = 0, 0
+for num in numbers: 
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1 
+print("Even number count is: ", even_count)
+print("Odd number count is : ", odd_count)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
